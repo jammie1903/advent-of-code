333711f1d10ed420096418c61454317c47d89ae5
Jamie Pennell <jamie.pennell@ripjar.com>
2023-12-17 10:34:46 +0000

    progress

--- src/day12/part2.js
+++ src/day12/part2.js
@@ -10,28 +10,35 @@ module.exports = input => {
       };
     });
   let total = 0;
-  lines.forEach(({ data, pattern }, index) => {
+  lines.forEach(({ data, pattern }) => {
     const possibleSpringGroups = data.split(/\.+/).filter(Boolean);
-    const comparison = index ? "includes" : "startsWith"
     console.log(pattern, possibleSpringGroups)
     const highestGroupSize = pattern.split(",").map(Number).reduce((a, b) => Math.max(a, b))
     console.log({highestGroupSize})
+    let comparison = "startsWith"
+
     const sequentialGroups = possibleSpringGroups.reduce((acc, group) => {
       const sectionLengths = group.split("?").map(s => s.length);
-
-      let possibilities = { [sectionLengths[0]]: 1 }
+      console.log(sectionLengths)
+      let possibilities = { [sectionLengths[0] || ""]: 1 }
       sectionLengths.slice(1).forEach(l => {
         const newPoss = {}
         Object.entries(possibilities).forEach(([p, count]) => {
-          const arr = p.split(",").map(Number);
+          const arr = p.split("");
           const copy = [...arr];
-          if(l)
-            arr.push(l)
-          copy[copy.length - 1] += l + 1;
+          if (/\d/.test(copy[copy.length - 1])) {
+            arr.push(",")
+            copy[copy.length - 1] = Number(copy[copy.length - 1]) + l + 1;
+          } else {
+            copy.push(l + 1);
+          }
+
           [arr, copy].forEach(sequence => {
-            if(sequence[sequence.length - 1] > highestGroupSize) return;
-            const str = sequence.join(",")
-            if(pattern[comparison](str.substring(0, str.length - 2))) {
+            const endsInNumber = /\d/.test(sequence[sequence.length - 1])
+            if (endsInNumber && Number(sequence[sequence.length - 1]) > highestGroupSize) return;
+            const str = sequence.join("");
+            const compString = endsInNumber ? str.substring(0, str.length - 1) : str;
+            if(pattern[comparison](compString)) {
               newPoss[str] ||= 0;
               newPoss[str] += count;
             }
@@ -46,7 +53,16 @@ module.exports = input => {
         //   return pattern[comparison](readyString.substring(0, readyString.length - 2));
         // })
       });
-      console.log(possibilities)
+
+      possibilities = Object.entries(possibilities).reduce((acc, [sequence, count]) => {
+        const trimmedSequence = sequence.endsWith(",") ? sequence.substring(0, sequence.length - 1) : sequence;
+        acc[trimmedSequence] ||= 0;
+        acc[trimmedSequence] += count;
+        return acc;
+      }, {})
+      // console.log(possibilities)
+
+
       // possibilities = possibilities.map(p => p.join(","));
       // let possibilities = [[sectionLengths[0]]]
       // sectionLengths.slice(1).forEach(l => {
@@ -126,9 +142,10 @@ module.exports = input => {
           }
         })
       })
+      comparison = "includes"
       return returnObject
     }, { });
-    console.log(sequentialGroups)
+    console.log("result", sequentialGroups[pattern])
     total += sequentialGroups[pattern] || 0;
     // total += possibilities.filter(p =>
     //   p.split(/\.+/).filter(Boolean).map(s => s.length).join(",") === pattern
