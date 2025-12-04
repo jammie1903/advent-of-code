620a6f8dc585bbce192ea910a56f51ef853fc112
Jamie Pennell <jamie.pennell@ripjar.com>
2025-12-04 09:52:13 +0000

    2025 day 4

---
+++ src/2025/day4/part2.py
@@ -0,0 +1,44 @@
+import sys
+content = sys.argv[1]
+
+grid = content.split("\n")
+total = 0
+
+def getCellValue(x, y):
+  if (y < 0 or y >= len(grid)):
+    return None
+  if (x < 0 or x >= len(grid[y])):
+    return None
+  return grid[y][x]
+
+def getAdjacentCellValues(x, y):
+  return [
+    getCellValue(x - 1, y - 1),
+    getCellValue(x - 1, y),
+    getCellValue(x - 1, y + 1),
+    getCellValue(x, y - 1),
+    getCellValue(x, y + 1),
+    getCellValue(x + 1, y - 1),
+    getCellValue(x + 1, y),
+    getCellValue(x + 1, y + 1),
+  ]
+check = True
+while check:
+  check = False
+  for y in range(len(grid)):
+    max = 0
+    for x in range(len(grid[y])):
+      cellValue = grid[y][x]
+      if (cellValue != "@"): continue
+      adjacentCells = getAdjacentCellValues(x, y)
+      occupiedCellCount = 0
+      for cell in adjacentCells:
+        if (cell == "@"): occupiedCellCount += 1
+      if (occupiedCellCount < 4):
+        total += 1
+        grid[y] = grid[y][:x] + "x" + grid[y][x+1:]
+        check = True
+    y += 1
+
+print("result", total)
+sys.stdout.flush()
